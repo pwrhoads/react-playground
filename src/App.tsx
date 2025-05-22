626e39af1f584e9089084fc085b7f21b31f46b69
Parker Rhoads <parkerwrhoads@gmail.com>
2025-05-22 17:20:43 -0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,22 @@ import "./App.css";
 import Sidebar from "./Components/Sidebar";
 
 function App() {
+  const [isSidebarOnLeft, setIsSidebarOnLeft] = useState<boolean>(true);
   return (
-    <>
-      <div className="flex h-screen w-screen">
-        <Sidebar />
+    <div className="flex h-screen w-screen">
+      <Sidebar leftAlign={isSidebarOnLeft} />
+      <div className="flex-1 flex justify-center items-center order-2">
+        <button
+          className="flex h-10 w-40 items-center"
+          //Trying to understand what all of this does
+          //onClick={} ---When mouse button is clicked, do everything between the curly braces
+          // () => ---We call a function that has no parameters Everything after the arrow is what's returned from the function
+          onClick={() => setIsSidebarOnLeft((prev) => !prev)}
+        >
+          Toggle Sidebar
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -5,20 +5,11 @@ interface SidebarProps {
   leftAlign?: boolean;
 }
 
-  const newAlignment = () => {
-    setAlignment({
-        alignment === "left" ? "right" : "left"
-    });
-
-//Probably a better way to do this
-const [alignment, setAlignment] = useState("left");
-
 const Sidebar = ({ leftAlign = true }: SidebarProps) => {
-
   return (
     <div
       className={`flex w-72 h-screen bg-blue-500 border-2 border-black ${
-        leftAlign === "true" ? "mr-auto" : "ml-auto"
+        leftAlign === true ? "order-1" : "order-3"
       }`}
     >
       Hello world
