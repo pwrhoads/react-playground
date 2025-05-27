ec7a934bc879a0b5cc03eada0f919be70e49eadb
Parker Rhoads <parkerwrhoads@gmail.com>
2025-05-27 13:33:34 -0400

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import {
+  useState,
+  // useEffect,
+  // useMemo,
+  // useRef,
+  // useContext,
+  // useCallback,
+} from "react";
 import "./App.css";
 import Sidebar from "./Components/Sidebar";
 
 function App() {
+  const ary = ["one", "two"];
+
   const [isSidebarOnLeft, setIsSidebarOnLeft] = useState<boolean>(true);
   return (
     <div className="flex h-screen w-screen">
       <Sidebar leftAlign={isSidebarOnLeft} />
-      <div className="flex-1 flex justify-center items-center order-2">
+      <div className="flex flex-col justify-center items-center order-2 grow">
         <button
-          className="flex h-10 w-40 items-center"
-          //Trying to understand what all of this does
-          //onClick={} ---When mouse button is clicked, do everything between the curly braces
-          // () => ---We call a function that has no parameters Everything after the arrow is what's returned from the function
+          className="flex h-10 w-40 items-center shrink-0"
           onClick={() => setIsSidebarOnLeft((prev) => !prev)}
         >
           Toggle Sidebar
         </button>
+        {ary.map((item) => {
+          return (
+            <div className="whitespace-pre" key={item}>
+              {item}{" "}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { memo, useEffect, useState } from "react";
 import { FaAngleRight } from "react-icons/fa";
 
 interface SidebarProps {
@@ -6,15 +6,43 @@ interface SidebarProps {
 }
 
 const Sidebar = ({ leftAlign = true }: SidebarProps) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const [welcomeMsg, setWelcomeMsg] = useState<string>(
+    "Sidebar is on the left"
+  );
+
+  //Proof of Concept for useEffect. Much better ways to handle this
+  useEffect(() => {
+    if (!leftAlign) {
+      setWelcomeMsg("Sidebar is on the right");
+    } else {
+      setWelcomeMsg("Sidebar is on the left");
+    }
+  }, [leftAlign]);
+
   return (
     <div
-      className={`flex w-72 h-screen bg-blue-500 border-2 border-black ${
-        leftAlign === true ? "order-1" : "order-3"
+      className={`flex ${
+        open ? "w-72" : "w-20"
+      } h-screen bg-red-500 border-2 border-black ${
+        leftAlign ? "order-1" : "order-3"
       }`}
     >
-      Hello world
+      <div className={`flex h-10 grow items-center`}>
+        {open && (
+          <h3 className="flex justify-center grow order-2">{welcomeMsg}</h3>
+        )}
+        <FaAngleRight
+          className={`flex cursor-pointer h-10 w-10 ${
+            leftAlign ? "ml-auto order-3" : "mr-auto order-1"
+          }  ${
+            (leftAlign && open) || (!leftAlign && !open) ? "rotate-180" : ""
+          }`}
+          onClick={() => setOpen((prev) => !prev)}
+        />
+      </div>
     </div>
   );
 };
 
-export default Sidebar;
+export default memo(Sidebar);
